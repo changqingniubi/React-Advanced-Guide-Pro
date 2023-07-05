6dd66d1b25bef388a42d4eb1c195d8a3b52a71dd
changqing <qing.chang@eeoa.com>
2023-07-05 16:36:10 +0800

    big

--- src/page/bigData/index.js
+++ src/page/bigData/index.js
@@ -84,65 +84,65 @@ function Circle({ position }){
 //     }
 // }
 // TODO: 改造方案
-class Index extends React.Component{
-    state={
-        dataList:[],    //数据源列表
-        renderList:[],  //渲染列表
-        position:{ width:0,height:0 }, // 位置信息
-        eachRenderNum:500  // 每次渲染数量
-    }
-    box = React.createRef()
-    componentDidMount(){
-        const { offsetHeight , offsetWidth } = this.box.current
-        const originList = new Array(20000).fill(1)
-        const times = Math.ceil(originList.length / this.state.eachRenderNum) /* 需要渲染此次数*/
-        let index = 1
-        this.setState({
-            dataList:originList,
-            position: { height:offsetHeight,width:offsetWidth }
-        },()=>{
-            this.toRenderList(index,times)
-        })
-    }
-    toRenderList=(index,times)=>{
-        if(index===times) return
-        const { renderList } = this.state
-        renderList.push(this.renderNewList(index))
-        this.setState({
-            renderList
-        })
-        setTimeout(()=>{ /* 用 requestIdleCallback 代替 setTimeout */
-            this.toRenderList(++index,times)
-        },0)
-    }
-    renderNewList(index){  /* 得到最新的渲染列表 */
-        const { dataList , position , eachRenderNum } = this.state
-        const list = dataList.slice((index-1) * eachRenderNum , index * eachRenderNum  )
-        return <React.Fragment key={index} >
+// class Index extends React.Component{
+//     state={
+//         dataList:[],    //数据源列表
+//         renderList:[],  //渲染列表
+//         position:{ width:0,height:0 }, // 位置信息
+//         eachRenderNum:500  // 每次渲染数量
+//     }
+//     box = React.createRef()
+//     componentDidMount(){
+//         const { offsetHeight , offsetWidth } = this.box.current
+//         const originList = new Array(20000).fill(1)
+//         const times = Math.ceil(originList.length / this.state.eachRenderNum) /* 需要渲染此次数*/
+//         let index = 1
+//         this.setState({
+//             dataList:originList,
+//             position: { height:offsetHeight,width:offsetWidth }
+//         },()=>{
+//             this.toRenderList(index,times)
+//         })
+//     }
+//     toRenderList=(index,times)=>{
+//         if(index===times) return
+//         const { renderList } = this.state
+//         renderList.push(this.renderNewList(index))
+//         this.setState({
+//             renderList
+//         })
+//         setTimeout(()=>{ /* 用 requestIdleCallback 代替 setTimeout */
+//             this.toRenderList(++index,times)
+//         },0)
+//     }
+//     renderNewList(index){  /* 得到最新的渲染列表 */
+//         const { dataList , position , eachRenderNum } = this.state
+//         const list = dataList.slice((index-1) * eachRenderNum , index * eachRenderNum  )
+//         return <React.Fragment key={index} >
 
-            {
-                list.map((item,index)=>{
-                    return <Circle key={index}
-                        position={position}
-                           />
-                })
-            }
-        </React.Fragment>
-    }
+//             {
+//                 list.map((item,index)=>{
+//                     return <Circle key={index}
+//                         position={position}
+//                            />
+//                 })
+//             }
+//         </React.Fragment>
+//     }
 
-    render(){
-         return <div className="bigData_index"
-             ref={this.box}
-                >
-            {this.state.renderList}
-         </div>
-    }
-}
+//     render(){
+//          return <div className="bigData_index"
+//              ref={this.box}
+//                 >
+//             {this.state.renderList}
+//          </div>
+//     }
+// }
 
 
 let eachRenderNum = 500;
 
-function Index2() {
+function Index() {
   const listRef = React.useRef({ list: [] });
   const [, setForceUpdate] = useState({});
   const [dataList, setDataList] = useState(() => {
