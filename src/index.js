1a09e3ec93a74ae99b9795561607e6de177103a0
changqing <qing.chang@eeoa.com>
2023-07-05 16:18:08 +0800

    first

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+// import './assets/styles/common.scss'
+
+
+
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById('app')
+)
